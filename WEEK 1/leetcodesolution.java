5a16ef861a00acb7f9d0ff5a497576e4388cd868
Apoorvaneja <apoorv45321@gmail.com>
2026-08-09 10:58:49 +0530

    Add files via upload

---
+++ WEEK 1/leetcodesolution.java
@@ -0,0 +1,18 @@
+public class leetcodesolution {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+
+       leetcodesolution obj = new leetcodesolution();
+
+        int num1 = 12;
+        int num2 = 5;
+
+        int result = obj.sum(num1, num2);
+
+        System.out.println("Sum = " + result);
+    }
+}
